2f423739ec2d528b4167482e0d009c2f7340107c
harshitsmit <156186341+harshitsmit@users.noreply.github.com>
2024-01-10 15:27:48 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,9 @@
+/**
+ * car
+ */
+public class car {
+
+    public static void main(String[] args) {
+        System.out.println(8*9);
+    }
+}
---
+++ discipline.java
@@ -0,0 +1,9 @@
+// my first program in java
+/*name- harshit
+10-01-2024
+ */
+public class discipline{
+    public static void main(String args[]){
+    System.out.println("this is my first java program");
+}
+}
